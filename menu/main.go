ea5e6c166926a8298f859a6661a3bb2bc9e66a1e
Thang <pxthang97@gmail.com>
2023-02-21 15:28:21 +0700

    dang ki tai khoan bang email

--- menu/main.go
+++ menu/main.go
@@ -3,36 +3,87 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net/mail"
 	"os"
 	"strconv"
+	"strings"
 )
 
+type User struct {
+	Email   string   `json:"email"`
+	Wallets []Wallet `json:"wallets"`
+}
+
+type Wallet struct {
+	Address string  `json:"address"`
+	Tokens  []Token `json:"tokens"`
+}
+
+type Token struct {
+	Symbol   string  `json:"symbol"`
+	Balances float64 `json:"balances"`
+}
+
 func main() {
-	XuatMenu()
+	// TODO : listUsers = DocListTuFile()
+	fmt.Println("===================He Thong Chuc Nang=======================")
+	fmt.Println("0. Xem list user hiện tại")
+	fmt.Println("1. Đăng ký bằng email.")
+	fmt.Println("============================================================")
+
+	var listUsers []User
 	for {
 		fmt.Print("Nhap chuc nang can thuc hien: ")
 		option := NhapOption()
-		if option == 9 {
-			fmt.Println(" Đang thoát chương trình...")
-			break
+		switch option {
+		case 0:
+			fmt.Println("List user hiện tại: ")
+			fmt.Println(listUsers)
+		case 1:
+			fmt.Print("Mời bạn nhập email: ")
+			newEmail := GetInput()
+			if !validEmail(newEmail) {
+				fmt.Println("Lỗi định dạnh email")
+				break
+			}
+			if !checkEmailExist(newEmail, listUsers) {
+				fmt.Println("Email đã tồn tại")
+				break
+			}
+			listUsers = append(listUsers, User{
+				Email:   newEmail,
+				Wallets: nil,
+			})
+			fmt.Println(listUsers)
+		default:
+			fmt.Println("Option bạn chọn không có trong menu. Vui lòng chọn lại")
+			fmt.Println("------------------------------------------------------")
 		}
-		XuOption(option)
-	}
 
-	fmt.Println("Bye")
+	}
+}
+
+func GetInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Đã xảy ra lỗi: ", err)
+		return ""
+	}
+	inputRemovedEnter := strings.Trim(input, "\r\n")
+	return inputRemovedEnter
 }
 
 func NhapOption() int {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
-	// handle
 	if err != nil {
 		fmt.Println("Đã xảy ra lỗi: ", err)
 		return -1
 	}
 
-	// convert string to int
-	option, err := strconv.Atoi(input[:len(input)-2])
+	inputRemovedEnter := strings.Trim(input, "\r\n")
+	option, err := strconv.Atoi(inputRemovedEnter)
 	if err != nil {
 		fmt.Println("Đã xảy ra lỗi: ", err)
 		return -1
@@ -40,33 +91,16 @@ func NhapOption() int {
 	return option
 }
 
-func XuatMenu() {
-	fmt.Println("===================He Thong Chuc Nang=======================")
-	fmt.Println("0. Tạo wallet")
-	fmt.Println("1. Xóa wallet")
-	fmt.Println("2. Hiển thị lại danh sách hệ thống chức năng")
-	fmt.Println("9. Để thoát chương trình")
-	fmt.Println("============================================================")
-	fmt.Println("Vui lòng chọn option từ 1 - 9")
+func validEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
 }
 
-func XuOption(option int) {
-	switch option {
-	case 0:
-		fmt.Println("Bạn đã chọn options: ", option)
-		fmt.Println("Chương trình đang thực hiện chức năng ...")
-		fmt.Println("Done")
-		fmt.Println("------------------------------------------")
-	case 1:
-		fmt.Println("Bạn đã chọn options: ", option)
-		fmt.Println("Chương trình đang thực hiện tải về ")
-		fmt.Println("Done")
-		fmt.Println("------------------------------------------")
-	case 2:
-		XuatMenu()
-	default:
-		fmt.Println("Option bạn chọn không có trong menu. Vui lòng chọn lại")
-		fmt.Println("------------------------------------------------------")
-		break
+func checkEmailExist(newEmail string, listUsers []User) bool {
+	for _, user := range listUsers {
+		if newEmail == user.Email {
+			return false
+		}
 	}
+	return true
 }
