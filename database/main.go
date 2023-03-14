c796f6d2ada3f41b6a89b302c53ef4025a8fdb62
Thang <pxthang97@gmail.com>
2023-03-14 15:30:41 +0700

    database section 1

---
+++ database/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
+
+func main() {
+	fmt.Println("Hello")
+}
+
+type User struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Wallet struct {
+	Address string    `json:"address"`
+	UserId  uuid.UUID `json:"user_id"`
+}
+
+type Token struct {
+	Address string `json:"address"`
+	Symbol  string `json:"symbol"`
+	Icon    string `json:"icon"`
+}
+
+type Transaction struct {
+	From    string  `json:"from"`
+	To      string  `json:"to"`
+	Address string  `json:"address"`
+	Amount  float64 `json:"amount"`
+}
